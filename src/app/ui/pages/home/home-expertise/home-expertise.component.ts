90b9a9be6fd5cd555c1822c26f79627ed60b32f3
Nihal Shah <nihalashah@yahoo.com>
2024-09-21 16:29:50 -0700

    experience-section changes wip

--- src/app/ui/pages/home/home-expertise/home-expertise.component.ts
+++ src/app/ui/pages/home/home-expertise/home-expertise.component.ts
@@ -38,9 +38,39 @@ export class HomeExpertiseComponent implements OnInit {
     public mediaObserver: MediaObserver,
     private scroll: ScrollDispatcher, private viewPortRuler: ViewportRuler) { }
 
+    
+    description1 = [
+      'During my time at Azilen Technologies, I specialized in developing and improving full-stack Spring Boot web applications, adhering to industry standards and best coding practices for high-quality software.',
+      'Key Contributions:',
+      '• Actively contributed to significant client projects, including Catastrophic Risk Modelling Enterprise and Platform for Legal Services and Judicial Support.',
+      '• Engineered and maintained a client portal housing over 10TB of natural disaster data, improving user accessibility by 30% and streamlining information management.',
+      '• Optimized the portal’s design, reducing user support tickets by 25% and enhancing the efficiency of the support process.',
+      '• Revamped a legal services platform with an innovative pricing model, enhancing user experience and reducing client workload by 20 work-hours per week.',
+      '• Developed seamless server deployment processes using containerization, achieving a 50% faster deployment time while maintaining system stability, now utilized by 15+ developers.',
+      '• Optimized APIs to process over 1 million requests per second, supporting a user base of 700,000+ concurrent users and improving the user experience by 30%.',
+      '• This comprehensive experience sharpened my skills in Java, Spring Boot, Liferay, AngularJS, and MySQL.'
+    ];
+
+    description2 = [
+      'During my internship at Azilen Technologies, I focused on implementing robust software solutions and optimizing operational processes.',
+      'Key Contributions:',
+      '• Access Control Implementation: Designed and implemented a comprehensive access control solution for BitBucket repositories, effectively eliminating unauthorized access. This initiative ensured ISO compliance, leading to a 40% reduction in security incidents and the establishment of a new protocol for tracking permissions.',
+      '• Process Optimization: Streamlined IT workflows, achieving a 70% reduction in manual tasks and enhancing overall operational efficiency by 45%.',
+      '• Module Migration: Led the migration of the Timesheet module from Node.js to Spring Boot, resulting in a 30% improvement in performance and operational efficiency.'
+];
+
+    description3 = [
+      'During my internship at Tech Celerity Global Solutions Pvt Limited, I focused on developing a robust E-commerce platform using Java and AngularJS, complemented by efficient MySQL database management.',
+      'Key Contributions:',
+      '• Developed user-friendly, responsive interfaces to enhance the overall shopping experience, leading to improved customer satisfaction and engagement.',
+      '• Played a pivotal role in API development, ensuring seamless data processing and maintaining data accuracy across the platform.',
+      '• Collaborated effectively with cross-functional teams, fostering an environment of innovation and efficiency.',
+      '• Actively participated in code reviews, contributing to the maintenance of high coding standards and best practices.',
+      '• This experience honed my technical skills in Java, AngularJS, and MySQL, while also enhancing my ability to work collaboratively in a fast-paced development environment.'
+  ];
+  
   ngOnInit(): void {
   }
-
 
 
   ngAfterViewInit(): void {
